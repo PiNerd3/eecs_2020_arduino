a69b99d10b76d0f53ae375faeebf7aa2d7dfd181
PiNerd3 <solonh@gmail.com>
2020-04-12 17:18:45 -0700

    Change Nicer Dice to be a bit better; write a greensleeves sketch

--- Nicer_Dice_code/Nicer_Dice_code.ino
+++ Nicer_Dice_code/Nicer_Dice_code.ino
@@ -2,46 +2,61 @@ int spots = 0;
 int outpin = 0;
 long ignore = 0;
 
+const int LED_3 = 13;
+const int LED_2 = 12;
+const int LED_1 = 6;
+const int LED_4 = 8;
+const int BUTTON_0 = 11;
+
 void setup() {
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(BUTTON_0, INPUT_PULLUP);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
   ignore = 2000 + millis();
+
+//  Serial.begin(9600);
 }
 
 void loop() {
-  for (outpin = 1; outpin < 5; outpin++) {
-    digitalWrite(outpin, LOW);
-  }
+//  for (outpin = 1; outpin < 5; outpin++) {
+//    digitalWrite(outpin, LOW);
+//  }
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, LOW);
 
   spots = random(1, 7);
 
+//  Serial.println(spots);
+
   if (spots == 6) {
-    digitalWrite(1, HIGH);
+    digitalWrite(LED_1, HIGH);
   }
+  
   if (spots == 1 || spots == 3 || spots == 5) {
-    digitalWrite(2, HIGH);
+    digitalWrite(LED_2, HIGH);
   }
   if (spots > 3) {
-    digitalWrite(3, HIGH);
+    digitalWrite(LED_3, HIGH);
   }
   if (spots > 1) {
-    digitalWrite(4, HIGH);
+    digitalWrite(LED_4, HIGH);
   }
 
   delay(20);
 
-  if (millis() > ignore && digitalRead(0) == LOW) {
+  if (millis() > ignore && digitalRead(BUTTON_0) == LOW) {
     checkbutton();
   }
 }
 
 void checkbutton() {
   delay(50);
-  while (digitalRead(0) == LOW) {}
+  while (digitalRead(BUTTON_0) == LOW) {}
   delay(50);
-  while (digitalRead(0) == HIGH) {}
+  while (digitalRead(BUTTON_0) == HIGH) {}
   ignore = 2000 + millis();
 }
---
+++ greensleeves/greensleeves.ino
@@ -0,0 +1,270 @@
+// #include "pitches.h"
+
+/*************************************************
+ * Public Constants
+ *************************************************/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+#define NOTE_R5 5000
+
+const int PASSIVE_BUZZER = 5;
+const int BPM_POT = A0; // A0
+
+const double BPM = 160.0;
+const int MILLISECONDS_PER_SIXTEENTH = (int)((60.0 * 1000.0) / (BPM * 2.0));
+
+int getMillisecondsPerSixteenth(int baseBPM = 140, int topBPM = 180) {
+  int BPM_val = analogRead(BPM_POT);
+  return (int)((BPM_val - 0) / (1023 - 0) * (topBPM - baseBPM) + baseBPM);
+};
+
+struct Note {
+  int pitch;
+  int sixteenth_duration;
+  int millis_duration;
+  bool is_rest;
+
+  int note_number;
+  
+  static int note_count;
+  
+  Note(int pitch, int duration) {
+    if (pitch == NOTE_R5) {
+      this->is_rest = true;
+    } else {
+      this->is_rest = false;
+      this->pitch = pitch;
+    }
+    
+    this->sixteenth_duration = duration;
+    this->millis_duration = duration * MILLISECONDS_PER_SIXTEENTH;
+
+    this->note_number = this->note_count++;
+  }
+
+  void play() {
+    int millis_to_play = this->sixteenth_duration * getMillisecondsPerSixteenth();
+    
+//    if (!this->is_rest) {
+//      tone(PASSIVE_BUZZER, this->pitch, this->millis_duration);
+//      delay(this->millis_duration + 5);
+//    } else {
+//      delay(this->millis_duration);
+//    }
+
+    if (!this->is_rest) {
+      tone(PASSIVE_BUZZER, this->pitch, millis_to_play);
+      delay(millis_to_play + 5);
+    } else {
+      delay(millis_to_play);
+    }
+
+  }
+};
+
+int Note::note_count = 0;
+
+//Note* make_note(int pitch, int duration) {
+//  Note* ret = new Note(pitch, duration);
+//  ret->pitch = pitch;
+//  ret->sixteenth_duration = duration;
+//  return ret;
+//};
+
+// Notes in the melody.
+Note greensleeves[] = {
+  Note(NOTE_R5, 10),
+  Note(NOTE_E5, 2),
+  Note(NOTE_G5, 4),
+  Note(NOTE_A5, 2),
+  Note(NOTE_B5, 3),
+  Note(NOTE_C6, 1),
+  Note(NOTE_B5, 2),
+  Note(NOTE_A5, 4),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_D5, 3),
+  Note(NOTE_E5, 1),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_G5, 4),
+  Note(NOTE_E5, 2),
+  Note(NOTE_E5, 3),
+  Note(NOTE_DS5, 1),
+  Note(NOTE_E5, 2),
+  Note(NOTE_FS5, 4),
+  Note(NOTE_DS5, 2),
+  Note(NOTE_B4, 4),
+  Note(NOTE_E5, 2),
+  Note(NOTE_G5, 4),
+  Note(NOTE_A5, 2),
+  Note(NOTE_B5, 3),
+  Note(NOTE_C6, 1),
+  Note(NOTE_B5, 2),
+  Note(NOTE_A5, 4),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_D5, 3),
+  Note(NOTE_E5, 1),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_G5, 3),
+  Note(NOTE_FS5, 1),
+  Note(NOTE_E5, 2),
+  Note(NOTE_DS5, 3),
+  Note(NOTE_CS5, 1),
+  Note(NOTE_DS5, 2),
+  Note(NOTE_E5, 6),
+  Note(NOTE_E5, 4),
+  Note(NOTE_R5, 2),
+  Note(NOTE_D6, 6),
+  Note(NOTE_D6, 3),
+  Note(NOTE_CS6, 1),
+  Note(NOTE_B5, 2),
+  Note(NOTE_A5, 4),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_D5, 3),
+  Note(NOTE_E5, 1),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_G5, 4),
+  Note(NOTE_E5, 2),
+  Note(NOTE_E5, 3),
+  Note(NOTE_DS5, 1),
+  Note(NOTE_E5, 2),
+  Note(NOTE_FS5, 4),
+  Note(NOTE_DS5, 2),
+  Note(NOTE_B4, 6),
+  Note(NOTE_D6, 6),
+  Note(NOTE_D6, 3),
+  Note(NOTE_CS6, 1),
+  Note(NOTE_B5, 2),
+  Note(NOTE_A5, 4),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_D5, 3),
+  Note(NOTE_E5, 1),
+  Note(NOTE_FS5, 2),
+  Note(NOTE_G5, 3),
+  Note(NOTE_FS5, 1),
+  Note(NOTE_E5, 2),
+  Note(NOTE_DS5, 3),
+  Note(NOTE_CS5, 1),
+  Note(NOTE_DS5, 2),
+  Note(NOTE_E5, 6),
+  Note(NOTE_E5, 4),
+  Note(NOTE_R5, 2)
+};
+
+const int GREENSLEEVES_LENGTH = sizeof(greensleeves) / sizeof(Note);
+
+void setup() {
+//  pinMode(ACTIVE_BUZZER, OUTPUT);
+
+  pinMode(BPM_POT, INPUT);
+  
+  Serial.begin(9600);
+  Serial.println(GREENSLEEVES_LENGTH);
+  Serial.println(MILLISECONDS_PER_SIXTEENTH);
+}
+
+void loop() {
+  Serial.println("starting loop");
+  
+  for (int i = 0; i < GREENSLEEVES_LENGTH; i++) {
+    Serial.println("starting note");
+    
+    greensleeves[i].play();
+//    Serial.println(greensleeves[i].note_number);
+//    Serial.println(greensleeves[i].sixteenth_duration);
+//    Serial.println(greensleeves[i].millis_duration);
+
+//    delay(200);
+  }
+   
+  // restart after two seconds 
+//  delay(5000);
+}
