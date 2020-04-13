a511c1bf62bd85cb75df8e2cec2fdb8f51c08fc7
PiNerd3 <solonh@gmail.com>
2020-04-13 12:43:07 -0700

    factored out a couple of files and it's working again. much smaller memory usage lmao

---
+++ greensleeves/Note.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "pitches.h"
+
+struct Note {
+  int pitch;
+  int duration_sixteenths; // Length in sixteenths
+  bool is_rest;
+
+  int note_number;
+  
+  static int note_count;
+  
+  Note(int pitch, int duration) {
+    if (pitch == NOTE_R) {
+      this->is_rest = true;
+    } else {
+      this->is_rest = false;
+      this->pitch = pitch;
+    }
+    
+    this->duration_sixteenths = duration;
+
+    this->note_number = this->note_count++; // Keep track of location in the melody.
+  }
+};
+
+int Note::note_count = 0; // Needs to be initialized here.
+
+int millisecondsPerSixteenthAtBPM(int BPM) {
+  return (int)((60.0 * 1000.0) / (BPM * 2.0));
+};
+
+void play(Note* note, int pin, int BPM) {
+    const int millis_to_play = note->duration_sixteenths * millisecondsPerSixteenthAtBPM(BPM);
+    
+    if (!note->is_rest) {
+      tone(pin, note->pitch, millis_to_play);
+    }
+    
+    delay(millis_to_play + 5);
+  }
