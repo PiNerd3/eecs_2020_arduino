6bdc3d585f6d9c6e83ed128ec7e80c2e8fd90422
Nathaniel H <solonh@gmail.com>
2020-04-20 01:31:00 -0700

    preliminary changes; probably doesn't compile

--- greensleeves/greensleeves.ino
+++ greensleeves/greensleeves.ino
@@ -1,100 +1,9 @@
-// include "pitches.h":
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
-
-#define NOTE_R 5000 // Representing a rest. Just something unreachable.
+// Pitch definitions
+const int NOTE_B0 = 31; const int NOTE_C1 = 33; const int NOTE_CS1 = 35; const int NOTE_D1 = 37; const int NOTE_DS1 = 39; const int NOTE_E1 = 41; const int NOTE_F1 = 44; const int NOTE_FS1 = 46; const int NOTE_G1 = 49; const int NOTE_GS1 = 52; const int NOTE_A1 = 55; const int NOTE_AS1 = 58; const int NOTE_B1 = 62; const int NOTE_C2 = 65; const int NOTE_CS2 = 69; const int NOTE_D2 = 73; const int NOTE_DS2 = 78; const int NOTE_E2 = 82; const int NOTE_F2 = 87; const int NOTE_FS2 = 93; const int NOTE_G2 = 98; const int NOTE_GS2 = 104; const int NOTE_A2 = 110; const int NOTE_AS2 = 117; const int NOTE_B2 = 123; const int NOTE_C3 = 131; const int NOTE_CS3 = 139; const int NOTE_D3 = 147; const int NOTE_DS3 = 156; const int NOTE_E3 = 165; const int NOTE_F3 = 175; const int NOTE_FS3 = 185; const int NOTE_G3 = 196; const int NOTE_GS3 = 208; const int NOTE_A3 = 220; const int NOTE_AS3 = 233; const int NOTE_B3 = 247; const int NOTE_C4 = 262; const int NOTE_CS4 = 277; const int NOTE_D4 = 294; const int NOTE_DS4 = 311; const int NOTE_E4 = 330; const int NOTE_F4 = 349; const int NOTE_FS4 = 370; const int NOTE_G4 = 392; const int NOTE_GS4 = 415; const int NOTE_A4 = 440; const int NOTE_AS4 = 466; const int NOTE_B4 = 494; const int NOTE_C5 = 523; const int NOTE_CS5 = 554; const int NOTE_D5 = 587; const int NOTE_DS5 = 622; const int NOTE_E5 = 659; const int NOTE_F5 = 698; const int NOTE_FS5 = 740; const int NOTE_G5 = 784; const int NOTE_GS5 = 831; const int NOTE_A5 = 880; const int NOTE_AS5 = 932; const int NOTE_B5 = 988; const int NOTE_C6 = 1047; const int NOTE_CS6 = 1109; const int NOTE_D6 = 1175; const int NOTE_DS6 = 1245; const int NOTE_E6 = 1319; const int NOTE_F6 = 1397; const int NOTE_FS6 = 1480; const int NOTE_G6 = 1568; const int NOTE_GS6 = 1661; const int NOTE_A6 = 1760; const int NOTE_AS6 = 1865; const int NOTE_B6 = 1976; const int NOTE_C7 = 2093; const int NOTE_CS7 = 2217; const int NOTE_D7 = 2349; const int NOTE_DS7 = 2489; const int NOTE_E7 = 2637; const int NOTE_F7 = 2794; const int NOTE_FS7 = 2960; const int NOTE_G7 = 3136; const int NOTE_GS7 = 3322; const int NOTE_A7 = 3520; const int NOTE_AS7 = 3729; const int NOTE_B7 = 3951; const int NOTE_C8 = 4186; const int NOTE_CS8 = 4435; const int NOTE_D8 = 4699; const int NOTE_DS8 = 4978;
+const int NOTE_R = 5000; // Representing a rest. Just something unreachable.
 
 
 // Settings.
-
-const bool DEBUG = false;
 
 //  Pin assignments
 const int PASSIVE_BUZZER = 5;
@@ -107,24 +16,18 @@ const bool VARIABLE_BPM = false;
 const int LOWEST_VARIABLE_BPM = 130;
 const int HIGHEST_VARIABLE_BPM = 300;
 
-const int INTER_NOTE_DELAY = 10; // Mimic the action of a real musician with a minute period of silence between notes.
-
 
 // Utility and setup.
 
-int millisecondsPerSixteenthAtBPM(int BPM) {
+inline int millisecondsPerSixteenthAtBPM(int BPM) {
   return (int)((60.0 * 1000.0) / (BPM * 2.0));
 };
 
 const int MILLISECONDS_PER_SIXTEENTH = millisecondsPerSixteenthAtBPM(DEFAULT_BPM);
 
-int calculateMillisecondsPerSixteenthFromPot(const int baseBPM = LOWEST_VARIABLE_BPM, const int topBPM = HIGHEST_VARIABLE_BPM) {
+inline int calculateMillisecondsPerSixteenthFromPot(const int baseBPM = LOWEST_VARIABLE_BPM, const int topBPM = HIGHEST_VARIABLE_BPM) {
   const int pot_val = analogRead(BPM_POT);
   const double BPM_val = (pot_val / 1023.0) * (topBPM - baseBPM) + baseBPM;
-  if (DEBUG) {
-    Serial.println(pot_val);
-    Serial.println(BPM_val);
-  }
   return millisecondsPerSixteenthAtBPM((int)BPM_val);
 };
 
@@ -135,10 +38,6 @@ struct Note {
   int duration_sixteenths; // Length in sixteenths
   int duration_millis; // Length in milliseconds, at default BPM. Calculated.
   bool is_rest;
-
-  int note_number;
-  
-  static int note_count;
   
   Note(int pitch, int duration) {
     if (pitch == NOTE_R) {
@@ -150,40 +49,25 @@ struct Note {
     
     this->duration_sixteenths = duration;
     this->duration_millis = duration * MILLISECONDS_PER_SIXTEENTH;
-
-    this->note_number = this->note_count++; // Keep track of location in the melody.
   }
+};
 
-  void play(bool variable_speed = VARIABLE_BPM) {
+void play(Note* note, bool variable_speed = VARIABLE_BPM) {
     int millis_to_play;
     if (variable_speed) {
-      millis_to_play = this->duration_sixteenths * calculateMillisecondsPerSixteenthFromPot();
+      millis_to_play = note->duration_sixteenths * calculateMillisecondsPerSixteenthFromPot();
     } else {
-      millis_to_play = this->duration_millis;
+      millis_to_play = note->duration_millis;
     }
     
-    if (!this->is_rest) {
-      tone(PASSIVE_BUZZER, this->pitch, millis_to_play);
+    if (!note->is_rest) {
+      tone(PASSIVE_BUZZER, note->pitch, millis_to_play);
     }
     
-    delay(millis_to_play + INTER_NOTE_DELAY);
+    delay(millis_to_play + (int)(0.05 * (double)millis_to_play));
   }
 };
 
-int Note::note_count = 0; // Needs to be initialized here.
-
-/** `make_note` helper function.
- *  Doesn't work, for some reason.
- *  Compiler doesn't recognize type here even though it does in, say, the declaration of GREENSLEEVES.
-
-Note* make_note(int pitch, int duration) {
-  Note* ret = new Note(pitch, duration);
-  ret->pitch = pitch;
-  ret->sixteenth_duration = duration;
-  return ret;
-};
-
-*/
 
 Note GREENSLEEVES[] = {
   Note(NOTE_R, 10),
@@ -265,34 +149,18 @@ Note GREENSLEEVES[] = {
 
 const int GREENSLEEVES_LENGTH = sizeof(GREENSLEEVES) / sizeof(Note);
 
-void play_greensleeves() {
+void play_song() {
   for (int i = 0; i < GREENSLEEVES_LENGTH; i++) {
-    if (DEBUG) {
-      Serial.println("starting note");
-    }
-    
-    GREENSLEEVES[i].play();
-  
-    if (DEBUG) {
-      Serial.println(GREENSLEEVES[i].note_number);
-      Serial.println(GREENSLEEVES[i].duration_sixteenths);
-      Serial.println(GREENSLEEVES[i].duration_millis);
-    }
+    play(&(GREENSLEEVES[i]));
   }
 }
 
 void setup() {
   pinMode(BPM_POT, INPUT);
 
-  if (DEBUG) {
-    Serial.begin(9600);
-    Serial.println(GREENSLEEVES_LENGTH);
-    Serial.println(MILLISECONDS_PER_SIXTEENTH);
-  }
-
-  play_greensleeves();
+  play_song();
 }
 
 void loop() {
-//  play_greensleeves(); // A little quieter.
+//  play_song(); // A little quieter.
 }
